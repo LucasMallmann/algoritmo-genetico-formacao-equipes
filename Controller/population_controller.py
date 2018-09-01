0f6c3bd7ea264e2fd5654ad188ae1e55a3381710
Lucas Mallmann <lucasmallmann76@gmail.com>
2018-09-01 10:53:26 -0300

    Commit inicial. População inicial do algoritmo gerada com a API

---
+++ Controller/population_controller.py
@@ -0,0 +1,14 @@
+from genetic_algorithm.population import Population
+
+
+class PopulationController(object):
+    def build_population(self) -> Population:
+        """
+
+        :rtype: Population
+        """
+        # Está 'hardcoded'. Modificar para passar dinamicamente
+        total_groups = 10
+        population = Population(quantity_of_groups=total_groups)
+        return population
+
---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_restplus import Api, Resource
+
+from Controller.population_controller import PopulationController
+from helpers.my_json_encoder import MyJsonEncoder
+import json
+
+app = Flask(__name__)
+api = Api(app)
+
+
+@api.route('/get-population')
+class GetPopulation(Resource):
+    def get(self):
+        my_encoder = MyJsonEncoder()
+        population_controller = PopulationController()
+        population = population_controller.build_population()
+        result = my_encoder.encode(population).replace("\'", '"')
+
+        return json.loads(result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
